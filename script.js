c21bbcaa5f743b8fcfa9bb252f987125ba3b5ac8
Will-Creative <will@willmarag.org>
2024-11-07 10:57:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-/* gsap.registerPlugin(ScrollTrigger);
 
-document.querySelectorAll('.section').forEach((section, index) => {
-  gsap.fromTo(
-    section.querySelector('.content'),
-    { opacity: 0, y: 50 },
-    {
-      opacity: 1,
-      y: 0,
-      scrollTrigger: {
-        trigger: section,
-        start: 'top center',
-        end: 'bottom center',
-        toggleActions: 'play none none reverse',
-      },
-      duration: 1,
-    }
-  );
-}); */
