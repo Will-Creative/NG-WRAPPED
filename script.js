233ac290e919389f23220d06d7d2f1ddc3122ed4
Will-Creative <will@willmarag.org>
2024-11-07 10:47:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-AOS.init();
+gsap.registerPlugin(ScrollTrigger);
 
-
-window.addEventListener("scroll", () => {
-  const scrollPosition = window.scrollY; // Get the vertical scroll position
-  const windowHeight = window.innerHeight;
-
- 
-  if (scrollPosition < windowHeight) {
-    // If we are in the first screen area, make the background blue
-    document.body.style.backgroundColor = "#007BFF";
-  } else if (scrollPosition < 2 * windowHeight) {
-    // In the second area, change to a different color, like green
-    document.body.style.backgroundColor = "#28a745";
-  } else if (scrollPosition < 3 * windowHeight) {
-    // In the third area, change to another color, like orange
-    document.body.style.backgroundColor = "#fd7e14";
-  } else {
-    // Any further scrolling can transition to another color, like purple
-    document.body.style.backgroundColor = "#6f42c1";
-  }
+document.querySelectorAll('.section').forEach((section, index) => {
+  gsap.fromTo(
+    section.querySelector('.content'),
+    { opacity: 0, y: 50 },
+    {
+      opacity: 1,
+      y: 0,
+      scrollTrigger: {
+        trigger: section,
+        start: 'top center',
+        end: 'bottom center',
+        toggleActions: 'play none none reverse',
+      },
+      duration: 1,
+    }
+  );
 });
